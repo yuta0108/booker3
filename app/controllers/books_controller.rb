d3d6e4c9a9d7a162f38616ffe5aa20bc241bdfff
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-11-06 15:49:01 +0000

    [FINISH]user showまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,26 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.user_id = current_user.id
+      @book.save
+      redirect_to book_path(@book)
+    else
+      render "show"
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
+
   def index
+
+
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user.id = current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
+
+
+
 end
